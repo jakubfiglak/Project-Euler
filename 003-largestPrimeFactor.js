9143456293262a5f7122be96dcce55003ce966c4
Jakub Figlak <jakub.figlak@gmail.com>
2020-01-06 15:04:55 +0100

    Solve challange no.3

---
+++ 003-largestPrimeFactor.js
@@ -0,0 +1,19 @@
+function largestPrimeFactor(number) {
+  const primeFactors = [];
+
+  while (!(number % 2)) {
+    primeFactors.push(2);
+    number = number / 2;
+  }
+
+  for (let i = 3; i <= Math.sqrt(number); i += 2) {
+    while (!(number % i)) {
+      primeFactors.push(i);
+      number = number / i;
+    }
+  }
+
+  primeFactors.push(number);
+
+  return Math.max(...primeFactors);
+}
